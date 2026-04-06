986576d78cc5c3cf4a7e5f878f10fc3cddb53c11
Liza <lizavetaliza06@gmail.com>
2026-04-06 16:18:33 +0300

    lab 2: app_news pages and MySQL setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'app_news',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'faculty_news_db',
+        'USER': 'faculty_news_user',
+        'PASSWORD': 'faculty_news_pass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
