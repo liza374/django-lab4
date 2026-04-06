00b1aa4836e84be9e3b607d8f74244594e2d7dcf
Liza <lizavetaliza06@gmail.com>
2026-04-06 17:12:45 +0300

    lab 3: added Tag model and many-to-many relation

--- app_news/admin.py
+++ app_news/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Category, Tag, Article
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category', 'slug')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'slug', 'main_page', 'category')
+    list_filter = ('pub_date', 'main_page', 'category', 'tags')
+    search_fields = ('title', 'description', 'slug')
+    filter_horizontal = ('tags',)
+    prepopulated_fields = {'slug': ('title',)}
--- app_news/models.py
+++ app_news/models.py
@@ -1,3 +1,82 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    category = models.CharField(
+        'Категорія',
+        max_length=250,
+        help_text='Максимум 250 символів'
+    )
+    slug = models.SlugField('Слаг', blank=True)
+
+    class Meta:
+        verbose_name = 'Категорія для публікації'
+        verbose_name_plural = 'Категорії для публікацій'
+
+    def __str__(self):
+        return self.category
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Тег',
+        max_length=100,
+        unique=True,
+        help_text='Введіть назву тегу'
+    )
+    slug = models.SlugField('Слаг', blank=True)
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(
+        'Заголовок',
+        max_length=250,
+        help_text='Максимум 250 символів'
+    )
+    description = models.TextField(
+        'Опис',
+        blank=True
+    )
+    pub_date = models.DateTimeField(
+        'Дата публікації',
+        default=timezone.now
+    )
+    slug = models.SlugField(
+        'Слаг',
+        unique_for_date='pub_date'
+    )
+    main_page = models.BooleanField(
+        'Головна',
+        default=False,
+        help_text='Показувати на головній сторінці'
+    )
+    category = models.ForeignKey(
+        Category,
+        related_name='articles',
+        blank=True,
+        null=True,
+        verbose_name='Категорія',
+        on_delete=models.CASCADE
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        related_name='articles',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Стаття'
+        verbose_name_plural = 'Статті'
+
+    def __str__(self):
+        return self.title
